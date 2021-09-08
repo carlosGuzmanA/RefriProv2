1160c2c6a6c7b065dffc20eaa72d687a5867ea7c
Carlos Guzman <pall>
2021-09-08 01:03:32 -0300

    quitar back scroll

--- js/scripts.js
+++ js/scripts.js
@@ -52,32 +52,32 @@
         hideBulletsOnMobile: "off",
         hideArrowsOnMobile: "off"
     });
-    $('.tp-banner-video').show().revolution({
-        dottedOverlay: "none",
-        delay: 9000,
-        startheight: 700,
-        hideTimerBar: "on",
-        navigationType: "none",
-		  navigationStyle: "preview4",
-		  touchenabled: "on",
-		  swipe_velocity: 0.7,
-		  swipe_min_touches: 1,
-		  swipe_max_touches: 1,
-		  drag_block_vertical: false,
-	  	  keyboardNavigation: "on",
-		  fullScreen: "on",
-		  spinner: "spinner1",
-		  stopLoop: "off",
-		  stopAfterLoops: -1,
-	  	  stopAtSlide: -1,
-		  forceFullWidth: "off",
-		  fullScreenAlignForce: "off",
-		  minFullScreenHeight: "400",
-		  hideThumbsOnMobile: "off",
-		  hideNavDelayOnMobile: 1500,
-        hideBulletsOnMobile: "off",
-		  hideArrowsOnMobile: "off"
-    });
+    // $('.tp-banner-video').show().revolution({
+    //     dottedOverlay: "none",
+    //     delay: 9000,
+    //     startheight: 700,
+    //     hideTimerBar: "on",
+    //     navigationType: "none",
+	// 	  navigationStyle: "preview4",
+	// 	  touchenabled: "on",
+	// 	  swipe_velocity: 0.7,
+	// 	  swipe_min_touches: 1,
+	// 	  swipe_max_touches: 1,
+	// 	  drag_block_vertical: false,
+	//   	  keyboardNavigation: "on",
+	// 	  fullScreen: "on",
+	// 	  spinner: "spinner1",
+	// 	  stopLoop: "off",
+	// 	  stopAfterLoops: -1,
+	//   	  stopAtSlide: -1,
+	// 	  forceFullWidth: "off",
+	// 	  fullScreenAlignForce: "off",
+	// 	  minFullScreenHeight: "400",
+	// 	  hideThumbsOnMobile: "off",
+	// 	  hideNavDelayOnMobile: 1500,
+    //     hideBulletsOnMobile: "off",
+	// 	  hideArrowsOnMobile: "off"
+    // });
     
     /* -------------------
     Owl Slider callings
@@ -232,14 +232,14 @@
     /* -------------------
     Smooth scrolling to anchor
     ---------------------*/
-    // $('.to-section a,.btn-scroll').bind('click', function(event) {
-    //     var $anchor = $(this);
-    //     $('html, body').stop().animate({
-    //         scrollTop: $($anchor.attr('href')).offset().top - 54
-    //     }, 1000, 'easeInOutExpo');
-    //     event.preventDefault();
-    //     return false;
-    // });
+    $('.to-section a,.btn-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 54
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+        return false;
+    });
     /* -------------------
     Back to top button function
     ---------------------*/
